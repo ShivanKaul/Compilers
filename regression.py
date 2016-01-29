ab4885517d60966b909b8424993e3324e17a6534
ShivanKaul <shivankaul.1993@gmail.com>
2016-01-29 00:48:58 -0500

    Improve regression script to take in args

--- regression.py
+++ regression.py
@@ -1,23 +1,37 @@
 import fnmatch
 import os
 import glob
-# Usage: from previous directory, simply do `python extra-tests/regression.py`
+import sys
 
 success = '\033[92m'
 fail = '\033[91m'
 
-def run_test(file, status):
+def run_test(file):
 	cmd = "./minic < %s 2>/dev/null" % file
 	print "> %s : " % file
 	os.system(cmd)
 
-print "Running tests that should return VALID..."
-for root, dirnames, filenames in os.walk('TEST_PROGRAMS/VALID'):
-        for filename in fnmatch.filter(filenames, '*.min'):
-            run_test(os.path.join(root, filename), success)
-print
+def valid():
+	print "Running tests that should return VALID..."
+	for root, dirnames, filenames in os.walk('TEST_PROGRAMS/VALID'):
+	        for filename in fnmatch.filter(filenames, '*.min'):
+	            run_test(os.path.join(root, filename))
+	print
 
-print "Running tests that should return INVALID..."
-for root, dirnames, filenames in os.walk('TEST_PROGRAMS/INVALID'):
-        for filename in fnmatch.filter(filenames, '*.min'):
-            run_test(os.path.join(root, filename), fail)
+def invalid():
+	print "Running tests that should return INVALID..."
+	for root, dirnames, filenames in os.walk('TEST_PROGRAMS/INVALID'):
+	        for filename in fnmatch.filter(filenames, '*.min'):
+	            run_test(os.path.join(root, filename))
+
+if (len(sys.argv) == 1):
+	valid()
+	invalid()
+else:
+	if (sys.argv[1] == "-i"):
+		invalid()
+	elif (sys.argv[1] == "-v"):
+		valid()
+	else:
+		print "Script failed, man."
+		sys.exit()
