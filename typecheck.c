e78609bb016a35a590966432a8074760607fe6d2
ShivanKaul <shivankaul.1993@gmail.com>
2016-02-03 02:06:23 -0500

    Typechecking [WIP]

---
+++ typecheck.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "typecheck.h"
+
+void typeCheck(STMT *stmts) {
+	while (stmts) {
+		switch (stmts->)
+	}
+	stmts = stmts->next;
+	typeCheck(stmts);
+}
---
+++ typecheck.h
@@ -0,0 +1,1 @@
+void typeCheck(STMT *stmts);
